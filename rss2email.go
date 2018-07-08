3a4fc36a45f0ce1cab1b18f808ab9e5097b4ebcb
Steve Kemp <steve@steve.org.uk>
2018-07-08 10:08:21 +0300

    Send emails with both TEXT & HTML parts.
    This improves our usability by generating emails which contain
    both the HTML content of the remote RSS-feed, and a text/plain
    alternative.
    
    This closes #1.

--- rss2email.go
+++ rss2email.go
@@ -12,15 +12,19 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
 	"flag"
 	"fmt"
+	"html"
 	"io/ioutil"
+	"mime/quotedprintable"
 	"net/http"
 	"os/exec"
 	"strings"
+	"text/template"
 
 	"os"
 
@@ -35,16 +39,112 @@ var VERBOSE = false
 // VERSION is our version, as set via CI.
 var version = "master/unreleased"
 
+// Template is our text/template which is designed used to send an
+// email to the local user.  We're using a template such that we
+// can send both HTML and Text versions of the RSS feed item.
+var Template = `Content-Type: multipart/mixed; boundary=21ee3da964c7bf70def62adb9ee1a061747003c026e363e47231258c48f1
+From: {{.From}}
+To: {{.To}}
+Subject: [rss2email] {{.Subject}}
+Mime-Version: 1.0
+
+--21ee3da964c7bf70def62adb9ee1a061747003c026e363e47231258c48f1
+Content-Type: multipart/related; boundary=76a1282373c08a65dd49db1dea2c55111fda9a715c89720a844fabb7d497
+
+--76a1282373c08a65dd49db1dea2c55111fda9a715c89720a844fabb7d497
+Content-Type: multipart/alternative; boundary=4186c39e13b2140c88094b3933206336f2bb3948db7ecf064c7a7d7473f2
+
+--4186c39e13b2140c88094b3933206336f2bb3948db7ecf064c7a7d7473f2
+Content-Type: text/plain; charset=UTF-8
+Content-Transfer-Encoding: quoted-printable
+
+{{.Text}}
+--4186c39e13b2140c88094b3933206336f2bb3948db7ecf064c7a7d7473f2
+Content-Type: text/html; charset=UTF-8
+Content-Transfer-Encoding: quoted-printable
+
+{{.HTML}}
+--4186c39e13b2140c88094b3933206336f2bb3948db7ecf064c7a7d7473f2--
+
+--76a1282373c08a65dd49db1dea2c55111fda9a715c89720a844fabb7d497--
+--21ee3da964c7bf70def62adb9ee1a061747003c026e363e47231258c48f1--
+`
+
+// toQuotedPrintable will convert the given input-string to a
+// quoted-printable format.  This is required for our MIME-part
+// body.
+func toQuotedPrintable(s string) (string, error) {
+	var ac bytes.Buffer
+	w := quotedprintable.NewWriter(&ac)
+	_, err := w.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+	err = w.Close()
+	if err != nil {
+		return "", err
+	}
+	return ac.String(), nil
+}
+
 // SendMail is a simple function that emails the given address.
 //
 // This is done via `/usr/sbin/sendmail` rather than via the use of SMTP.
 //
-func SendMail(addr string, subject string, body string) error {
+// We send a MIME message with both a plain-text and a HTML-version of the
+// message.  This should be nicer for users.
+func SendMail(addr string, subject string, textstr string, htmlstr string) error {
+	var err error
+
+	//
+	// Ensure we have a recipient.
+	//
 	if addr == "" {
 		e := errors.New("Empty recipient address, is '$LOGNAME' set?")
 		fmt.Printf("%s\n", e.Error())
 		return e
 	}
+
+	//
+	// Here is a temporary structure we'll use to popular our email
+	// template.
+	//
+	type TemplateParms struct {
+		To      string
+		From    string
+		Text    string
+		HTML    string
+		Subject string
+	}
+
+	//
+	// Populate it appropriately.
+	//
+	var x TemplateParms
+	x.To = addr
+	x.From = "user@rss2email.invalid"
+	x.Text, err = toQuotedPrintable(textstr)
+	if err != nil {
+		return err
+	}
+	x.HTML, err = toQuotedPrintable(html.UnescapeString(htmlstr))
+	if err != nil {
+		return err
+	}
+	x.Subject = subject
+
+	//
+	// Render our template into a buffer.
+	//
+	src := string(Template)
+	t := template.Must(template.New("tmpl").Parse(src))
+	buf := &bytes.Buffer{}
+	err = t.Execute(buf, x)
+
+	//
+	// Prepare to run sendmail, with a pipe we can write our
+	// message to.
+	//
 	sendmail := exec.Command("/usr/sbin/sendmail", "-f", addr, addr)
 	stdin, err := sendmail.StdinPipe()
 	if err != nil {
@@ -52,33 +152,36 @@ func SendMail(addr string, subject string, body string) error {
 		return err
 	}
 
+	//
+	// Get the output pipe.
+	//
 	stdout, err := sendmail.StdoutPipe()
 	if err != nil {
 		fmt.Printf("Error sending email: %s\n", err.Error())
 		return err
 	}
 
-	// What we'll send
-	msg := ""
-	msg += "To: " + addr + "\n"
-	msg += "From: user@rss2email.invalid\n"
-	msg += "Subject: [rss2email] " + subject + "\n"
-	msg += "\n"
-	msg += body
-
+	//
+	// Run the command, and pipe in the rendered template-result
+	//
 	sendmail.Start()
-	_, err = stdin.Write([]byte(msg))
+	_, err = stdin.Write(buf.Bytes())
 	if err != nil {
 		fmt.Printf("Failed to write to sendmail pipe: %s\n", err.Error())
 		return err
 	}
 	stdin.Close()
+
+	//
+	// Read the output of Sendmail.
+	//
 	var out []byte
 	out, err = ioutil.ReadAll(stdout)
 	if err != nil {
 		fmt.Printf("Error reading mail output: %s\n", err.Error())
 		return nil
 	}
+
 	if VERBOSE {
 		fmt.Printf("%s\n", out)
 	}
@@ -191,7 +294,7 @@ func ProcessURL(input string) {
 			text := html2text.HTML2Text(i.Content)
 
 			// Send the email
-			err := SendMail(os.Getenv("LOGNAME"), i.Title, text)
+			err := SendMail(os.Getenv("LOGNAME"), i.Title, text, i.Content)
 
 			// Only then record this item as having been seen
 			if err == nil {
