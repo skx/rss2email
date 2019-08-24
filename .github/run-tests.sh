96cf5aa2c225965adbe4b520056fa98666c78062
Steve Kemp <steve@steve.org.uk>
2019-08-24 20:26:20 +0300

    Run staticcheck tests too

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-# Install the lint-tool, and the shadow-tool
+# Install tools to test our code-quality.
 go get -u golang.org/x/lint/golint
 go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
+go get -u honnef.co/go/tools/cmd/staticcheck
+
+# Run the static-check tool - we ignore errors in goserver/static.go
+t=$(mktemp)
+staticcheck -checks all ./... > $t
+if [ -s $t ]; then
+    echo "Found errors via 'staticcheck'"
+    cat $t
+    rm $t
+    exit 1
+fi
+rm $t
 
 # At this point failures cause aborts
 set -e
