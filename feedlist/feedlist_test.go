69844bf911c2f8cb94c14edd570f0da8fc291d17
Steve Kemp <steve@steve.org.uk>
2020-08-06 09:55:07 +0300

    Test deleting a feed-entry works

--- feedlist/feedlist_test.go
+++ feedlist/feedlist_test.go
@@ -17,7 +17,7 @@ func TestEmpty(t *testing.T) {
 	}
 }
 
-// TestSave ensures we create a file
+// TestSave ensures we create a file with an entry in it.
 func TestSave(t *testing.T) {
 
 	// Create a temporary file
@@ -47,5 +47,53 @@ func TestSave(t *testing.T) {
 	}
 	if found[0] != "https://example.com/foo.atom" {
 		t.Errorf("unexpected entry found: %s", found[0])
+	}
+}
+
+// TestDelete ensures we can delete an entry from a feedlist
+func TestDelete(t *testing.T) {
+	// Create a temporary file
+	file, err := ioutil.TempFile(os.TempDir(), "testsave")
+	if err != nil {
+		t.Fatalf("failed to make temporary file: %s", err.Error())
+	}
+	defer os.Remove(file.Name())
+
+	// Create a new feed with two entries
+	list := New(file.Name())
+	list.Add("https://example.com/foo.atom")
+	list.Add("https://example.com/foo.xml")
+
+	// Save it to disk
+	err = list.Save()
+	if err != nil {
+		t.Fatalf("failed to save feed list: %s", err)
+	}
+
+	// The file should now have contents - we can reload it
+	// and confirm the count is as-expected
+	updated := New(file.Name())
+	found := updated.Entries()
+
+	if len(found) != 2 {
+		t.Errorf("expected two entries, found %d", len(found))
+	}
+
+	// Now delete the atom entry & save
+	updated.Delete("https://example.com/foo.atom")
+	err = updated.Save()
+	if err != nil {
+		t.Fatalf("failed to save feed list: %s", err)
+	}
+
+	// If the delete worked we have a feedlist with a single
+	// entry present now.
+	final := New(file.Name())
+	if len(final.Entries()) != 1 {
+		t.Errorf("expected one entry, found %d", len(found))
+	}
+
+	if final.Entries()[0] != "https://example.com/foo.xml" {
+		t.Errorf("after deletion we have an unexpected entry")
 	}
 }
