f7bb82682b168bcb7f1e29c723cf67780e1d6ab4
Steve Kemp <steve@steve.org.uk>
2020-08-06 15:24:29 +0300

    Added more test-cases; 100% coverage

--- withstate/feeditem_test.go
+++ withstate/feeditem_test.go
@@ -55,5 +55,36 @@ func TestCollision(t *testing.T) {
 	if a.path() == b.path() {
 		t.Fatalf("two different objects have identical hashes/paths")
 	}
+}
 
+// TestCollisionMissingHome ensures that we can find the home-directory
+// of a user, even without the environment
+func TestCollisionMissingHome(t *testing.T) {
+
+	// Remove the environmental variable
+	cur := os.Getenv("HOME")
+	os.Setenv("HOME", "")
+
+	// So we want to have two feed items with the same
+	// GUID.  They should map to the same file, so we
+	// can confirm they would be treated as identical
+	a := &FeedItem{&gofeed.Item{}}
+	b := &FeedItem{&gofeed.Item{}}
+
+	a.GUID = "steve"
+	b.GUID = "steve"
+
+	if a.path() != b.path() {
+		t.Fatalf("two identical objects have different hashes/paths")
+	}
+
+	// Update to confirm that results in a change
+	b.GUID = "kemp"
+
+	if a.path() == b.path() {
+		t.Fatalf("two different objects have identical hashes/paths")
+	}
+
+	// Reset
+	os.Setenv("HOME", cur)
 }
