95372db65b6ac62c9ffdd14d5cb7b116f6ad8ddd
Steve Kemp <steve@steve.org.uk>
2021-04-04 18:11:03 +0300

    Fixed linter warning on function-comment

--- configfile/configfile.go
+++ configfile/configfile.go
@@ -65,7 +65,8 @@ func New() *ConfigFile {
 	return &ConfigFile{}
 }
 
-// New creates a configuration-file reader, using the given file as a source.
+// NewWithPath creates a configuration-file reader, using the given file as
+// a source.
 func NewWithPath(file string) *ConfigFile {
 	return &ConfigFile{path: file}
 }
