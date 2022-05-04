1011504ebeb9b7ccf186ca9a256e610389f11872
Steve Kemp <steve@steve.org.uk>
2022-05-04 05:55:58 +0300

    Use the Tag: field, if it is present.

--- template/template_test.go
+++ template/template_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestTemplate(t *testing.T) {
 	content := EmailTemplate()
-	if len(content) != 2265 {
+	if len(content) != 2292 {
 		t.Fatalf("unexpected template size 2265 != %d", len(content))
 	}
 }
