b31a6d80e7e6aadd53e86750bff96418ac2b88bf
Steve Kemp <steve@steve.org.uk>
2023-12-16 09:24:11 +0200

    Add slog setup.
    This sets up the basic stubs which can be used for later.

--- main.go
+++ main.go
@@ -6,24 +6,61 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/skx/subcommands"
 )
 
-//
+var (
+	// logger contains a shared logging handle, used by our sub-commands.
+	logger *slog.Logger
+)
+
 // Recovery is good
-//
 func recoverPanic() {
 	if r := recover(); r != nil {
-		fmt.Printf("recovered from panic while running %v\n%s\n", os.Args, r)
+		logger.Error("recovered from a panic", slog.String("error", fmt.Sprintf("%s", r)))
 	}
 }
 
-//
 // Register the subcommands, and run the one the user chose.
-//
 func main() {
+
+	//
+	// Setup our default logging level
+	//
+	lvl := new(slog.LevelVar)
+	lvl.Set(slog.LevelWarn)
+
+	//
+	// Allow showing "all the logs"
+	//
+	if os.Getenv("LOG_ALL") != "" {
+		lvl.Set(slog.LevelDebug)
+	}
+
+	// Those handler options
+	opts := &slog.HandlerOptions{
+		Level: lvl,
+	}
+
+	//
+	// Default to showing to STDERR in text.
+	//
+	var handler slog.Handler = slog.NewTextHandler(os.Stderr, opts)
+
+	//
+	// But allow JSON formatting too.
+	//
+	if os.Getenv("LOG_JSON") != "" {
+		handler = slog.NewJSONHandler(os.Stderr, opts)
+	}
+
+	//
+	// Create our logging handler, using the level we've just setup
+	//
+	logger = slog.New(handler)
 
 	//
 	// Catch errors
