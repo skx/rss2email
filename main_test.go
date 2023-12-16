c8157d4066a6fd17c575e324bdca2262a8113f04
Steve Kemp <steve@steve.org.uk>
2023-12-16 12:59:25 +0200

    Ensure we have a global logger-handle when running tests.

---
+++ main_test.go
@@ -0,0 +1,24 @@
+// main_test.go - just setup a logger so the test-cases have
+// one available.
+
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+// init runs at test-time.
+func init() {
+
+	// setup logging-level
+	lvl := new(slog.LevelVar)
+	lvl.Set(slog.LevelWarn)
+
+	// create a handler
+	opts := &slog.HandlerOptions{Level: lvl}
+	handler := slog.NewTextHandler(os.Stderr, opts)
+
+	// ensure the global-variable is set.
+	logger = slog.New(handler)
+}
