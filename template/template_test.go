57bab6310586c4d12c4317fb78bfe55230afc180
timendum <timedum@gmail.com>
2025-06-16 11:55:12 +0200

    Fix test on template

--- template/template_test.go
+++ template/template_test.go
@@ -8,7 +8,7 @@ func TestTemplate(t *testing.T) {
 
 	// content and expected length
 	content := EmailTemplate()
-	length := 2645
+	length := 2766
 
 	if len(content) != length {
 		t.Fatalf("unexpected template size %d != %d", length, len(content))
